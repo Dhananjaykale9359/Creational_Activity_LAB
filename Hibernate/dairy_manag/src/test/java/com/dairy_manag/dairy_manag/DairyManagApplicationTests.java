5816b8f4ffdff80aac7645915677ac1cda9aa2c0
Dhananjay Kale <161159213+Dhananjaykale9359@users.noreply.github.com>
2025-05-19 18:10:31 +0530

    Add files via upload

---
+++ Hibernate/dairy_manag/src/test/java/com/dairy_manag/dairy_manag/DairyManagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dairy_manag.dairy_manag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyManagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
